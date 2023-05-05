cd52e69772f4cc0f207977afd6db723ca4b0b5f8
rohan <80212018+rohan031@users.noreply.github.com>
2023-05-05 16:06:53 +0530

    create profile done

--- src/uniqueId/uniqueIdGen.js
+++
@@ -1,9 +0,0 @@
-function genUniqueId() {
-	const dateStr = Date.now().toString(36); // convert num to base 36 and stringify
-
-	const randomStr = Math.random().toString(36).substring(2, 8); // start at index 2 to skip decimal point
-
-	return `${dateStr}-${randomStr}`;
-}
-
-export default genUniqueId;
