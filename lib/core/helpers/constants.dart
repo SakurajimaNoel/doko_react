d9371a320418d7df3629d16b456b05ca81d38d98
rohan <rohanverma031@gmail.com>
2024-09-07 10:13:58 +0530

    feat: added app level constants

---
+++ lib/core/helpers/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const double padding = 16;
+  static const double gap = 16;
+  static const double appBarHeight = kToolbarHeight;
+}
