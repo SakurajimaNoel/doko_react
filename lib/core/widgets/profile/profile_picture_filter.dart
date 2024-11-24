5bd1af6bb82c10171945ec1555dd982ec5078770
rohan <rohanverma031@gmail.com>
2024-11-24 13:53:32 +0530

    feat: added widget for profile picture

---
+++ lib/core/widgets/profile/profile_picture_filter.dart
@@ -0,0 +1,34 @@
+import 'package:doko_react/core/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePictureFilter extends StatelessWidget {
+  const ProfilePictureFilter({
+    super.key,
+    this.child = const SizedBox.shrink(),
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final currTheme = Theme.of(context).colorScheme;
+
+    return Container(
+      alignment: Alignment.bottomLeft,
+      padding: const EdgeInsets.all(Constants.padding),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            currTheme.surface.withOpacity(0.25),
+            currTheme.surface.withOpacity(0.15),
+            currTheme.surface.withOpacity(0.15),
+            currTheme.surface.withOpacity(0.25),
+          ],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
