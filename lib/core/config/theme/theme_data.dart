2fd756f55bded8d3200e54b900ee1e9978cf7c9b
rohan <rohanverma031@gmail.com>
2024-11-24 13:52:58 +0530

    feat: added theme data

---
+++ lib/core/config/theme/theme_data.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GlobalThemeData {
+  static ThemeData themeData(ColorScheme colorScheme) {
+    return ThemeData(
+      colorScheme: colorScheme,
+      fontFamily: "Rubik",
+      useMaterial3: true,
+    );
+  }
+
+  // custom color
+  static ThemeData lightCustomThemeData(Color color) {
+    return themeData(
+      _lightCustom(color),
+    );
+  }
+
+  static ThemeData darkCustomThemeData(Color color) {
+    return themeData(
+      _darkCustom(color),
+    );
+  }
+
+  static ColorScheme _lightCustom(Color color) {
+    return ColorScheme.fromSeed(seedColor: color, brightness: Brightness.light);
+  }
+
+  static ColorScheme _darkCustom(Color color) {
+    return ColorScheme.fromSeed(seedColor: color, brightness: Brightness.dark);
+  }
+}
