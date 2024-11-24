700b1c553bd7a3ed1e3b1bf52af33a16b37355ea
rohan <rohanverma031@gmail.com>
2024-11-24 11:21:21 +0530

    feat: added complete profile info page

--- lib/features/complete-profile/presentation/pages/info/complete_profile_info_page.dart
+++ lib/features/complete-profile/presentation/pages/info/complete_profile_info_page.dart
@@ -1,4 +1,10 @@
+import 'package:doko_react/core/config/router/router_constants.dart';
+import 'package:doko_react/core/constants/constants.dart';
+import 'package:doko_react/core/helpers/display/display_helper.dart';
+import 'package:doko_react/core/widgets/heading/heading.dart';
+import 'package:doko_react/features/authentication/presentation/widgets/public/sign-out-button/sign_out_button.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CompleteProfileInfoPage extends StatefulWidget {
   const CompleteProfileInfoPage({
@@ -14,12 +20,188 @@ class CompleteProfileInfoPage extends StatefulWidget {
 }
 
 class _CompleteProfileInfoPageState extends State<CompleteProfileInfoPage> {
+  final formKey = GlobalKey<FormState>();
+  late final TextEditingController usernameController;
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController dobController =
+      TextEditingController(text: "Select date of birth");
+
+  final int years13ToDays = 4748;
+  DateTime? date;
+
+  @override
+  void initState() {
+    super.initState();
+
+    usernameController = TextEditingController(text: widget.username);
+  }
+
+  void handleProfileInfo() {
+    final isValid = formKey.currentState?.validate() ?? false;
+    if (!isValid || date == null) {
+      return;
+    }
+
+    context.pushNamed(
+      RouterConstants.completeProfilePicture,
+      pathParameters: {
+        "username": widget.username,
+        "name": nameController.text.trim(),
+        "dob": date.toString(),
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currTheme = Theme.of(context).colorScheme;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.username),
+        title: const Text("Profile Information"),
+        actions: const [
+          SignOutButton(),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(Constants.padding),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Heading.left(
+                    "Profile Information",
+                    size: Constants.largeFontSize,
+                  ),
+                  const Text(
+                      "Let's get started! Please fill in the information below to complete your profile. We're excited to have you join Doki."),
+                  const SizedBox(
+                    height: Constants.gap * 1.5,
+                  ),
+                  Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          enabled: false,
+                          controller: usernameController,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            labelText: "Username*",
+                            hintText: "Username...",
+                            disabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: currTheme.outline,
+                              ),
+                            ),
+                            labelStyle: TextStyle(
+                              color: currTheme.onSurface,
+                            ),
+                          ),
+                          style: TextStyle(
+                            color: currTheme.onSurface,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: Constants.gap * 1.25,
+                        ),
+                        TextFormField(
+                          controller: nameController,
+                          validator: (value) {
+                            return null;
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onChanged: (value) {},
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Name*",
+                            hintText: "Name...",
+                          ),
+                        ),
+                        const SizedBox(
+                          height: Constants.gap * 1.25,
+                        ),
+                        GestureDetector(
+                          onTap: selectDate,
+                          child: TextFormField(
+                            enabled: false,
+                            controller: dobController,
+                            validator: (value) {
+                              if (date == null) {
+                                return "Date of Birth is required.";
+                              }
+
+                              return null;
+                            },
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              labelText: "Date of Birth*",
+                              hintText: "DOB...",
+                              suffixIcon: Icon(
+                                Icons.calendar_month,
+                                color: currTheme.onSurface,
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: currTheme.outline,
+                                ),
+                              ),
+                              labelStyle: TextStyle(
+                                color: currTheme.onSurface,
+                              ),
+                            ),
+                            style: TextStyle(
+                              color: currTheme.onSurface,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            FilledButton(
+              onPressed: handleProfileInfo,
+              style: FilledButton.styleFrom(
+                minimumSize: const Size(
+                  Constants.buttonWidth,
+                  Constants.buttonHeight,
+                ),
+              ),
+              child: const Text("Next"),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> selectDate() async {
+    DateTime limDate = DateTime.now().subtract(Duration(
+      days: years13ToDays,
+    ));
+
+    final DateTime selected = await showDatePicker(
+          context: context,
+          initialDate: limDate,
+          firstDate: DateTime(1924),
+          lastDate: limDate,
+        ) ??
+        date ??
+        limDate;
+
+    if (selected != date) {
+      setState(() {
+        date = selected;
+        dobController.text = dateString(selected);
+        formKey.currentState?.validate();
+      });
+    }
   }
 }
