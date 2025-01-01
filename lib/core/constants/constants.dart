3b2687bd35a13f6935a96d481e330d83d2145cfe
rohan <rohanverma031@gmail.com>
2025-01-01 20:14:34 +0530

    feat: added duration for videos in story and post

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -43,7 +43,8 @@ class Constants {
   static int editProfileCachedHeight = (_root * 75).round();
 
   // video
-  static const Duration videoDuration = Duration(seconds: 90);
+  static const Duration videoDurationPost = Duration(seconds: 90);
+  static const Duration videoDurationStory = Duration(seconds: 30);
   static const double landscape = 16 / 9;
   static const double portrait = 9 / 16;
 
