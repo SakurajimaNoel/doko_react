7ca5ba67b5b9dcc62ae3b204ccb599d11c1bab72
rohan <rohanverma031@gmail.com>
2025-01-15 13:39:56 +0530

    feat: updated layout for media selection

--- lib/core/widgets/image-picker/image_picker_widget.dart
+++ lib/core/widgets/image-picker/image_picker_widget.dart
@@ -106,57 +106,70 @@ class ImagePickerWidget extends StatelessWidget {
   }
 
   void selectOptions(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    final currTheme = Theme.of(context).colorScheme;
+
     showModalBottomSheet(
       context: context,
       showDragHandle: true,
       builder: (BuildContext context) {
         return Container(
           height: Constants.height * 15,
+          width: width,
           padding: EdgeInsets.all(Constants.padding),
-          child: GridView.count(
-            mainAxisSpacing: Constants.gap,
-            crossAxisSpacing: Constants.gap,
-            childAspectRatio: 4 / 1,
-            crossAxisCount: 2,
+          child: Wrap(
+            alignment: WrapAlignment.spaceEvenly,
+            spacing: Constants.gap * 2,
+            runSpacing: Constants.gap * 2,
             children: [
-              if (image) ...[
-                SizedBox(
-                  child: FilledButton.tonalIcon(
-                    onPressed: () {
-                      handleGallery(context);
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.photo),
-                    label: const Text("Select image"),
-                  ),
-                ),
-                FilledButton.tonalIcon(
+              if (image)
+                _IconButtonWithBottomLabel(
+                  label: "Select image",
                   onPressed: () {
-                    handleCamera(context);
+                    handleGallery(context);
                     Navigator.pop(context);
                   },
-                  icon: const Icon(Icons.photo_camera),
-                  label: const Text("Take picture"),
-                )
-              ],
-              if (video) ...[
-                FilledButton.tonalIcon(
+                  icon: Icon(
+                    Icons.photo_library,
+                    color: currTheme.primary,
+                  ),
+                ),
+              if (video)
+                _IconButtonWithBottomLabel(
+                  label: "Select video",
                   onPressed: () {
                     handleVideo(context);
                     Navigator.pop(context);
                   },
-                  icon: const Icon(Icons.video_collection),
-                  label: const Text("Select video"),
+                  icon: Icon(
+                    Icons.video_collection,
+                    color: currTheme.primary,
+                  ),
                 ),
-                FilledButton.tonalIcon(
+              if (image)
+                _IconButtonWithBottomLabel(
+                  label: "Take picture",
+                  onPressed: () {
+                    handleCamera(context);
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.photo_camera,
+                    color: currTheme.secondary,
+                  ),
+                ),
+              if (video)
+                _IconButtonWithBottomLabel(
+                  label: "Record video",
                   onPressed: () {
                     handleVideoCapture(context);
                     Navigator.pop(context);
                   },
-                  icon: const Icon(Icons.videocam),
-                  label: const Text("Record video"),
+                  icon: Icon(
+                    Icons.videocam,
+                    color: currTheme.secondary,
+                  ),
                 ),
-              ],
             ],
           ),
         );
@@ -171,7 +184,10 @@ class ImagePickerWidget extends StatelessWidget {
     if (icon == null) {
       return TextButton(
         onPressed: disabled ? null : action,
-        child: Text(text!),
+        child: Text(
+          text!,
+          textAlign: TextAlign.center,
+        ),
       );
     }
 
@@ -241,5 +257,49 @@ class ImagePickerWidget extends StatelessWidget {
 
     if (selectedMedia == null) return;
     onSelection([selectedMedia]);
+  }
+}
+
+class _IconButtonWithBottomLabel extends StatelessWidget {
+  const _IconButtonWithBottomLabel({
+    required this.label,
+    required this.onPressed,
+    required this.icon,
+  });
+
+  final String label;
+  final VoidCallback onPressed;
+  final Icon icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: icon,
+        ),
+        _IconLabel(label),
+      ],
+    );
+  }
+}
+
+class _IconLabel extends StatelessWidget {
+  const _IconLabel(this.text);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: Constants.smallFontSize,
+      ),
+      textAlign: TextAlign.center,
+    );
   }
 }
