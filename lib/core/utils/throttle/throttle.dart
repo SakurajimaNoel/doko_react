8fa19756dc36c4cd7dc68fc4b812cfb78c155463
rohan <rohanverma031@gmail.com>
2025-01-28 22:27:19 +0530

    feat: corrected throttle for doki use case

--- lib/core/utils/throttle/throttle.dart
+++ lib/core/utils/throttle/throttle.dart
@@ -3,26 +3,38 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 
 class Throttle {
-  Duration interval;
+  final Duration interval;
   Timer? _timer;
-  bool _ready = false;
+  VoidCallback? latest;
 
   Throttle(this.interval);
 
   call(VoidCallback callback) {
-    if (!_ready) {
-      // If the first call, set _ready after interval
-      _timer = Timer(interval, () {
-        _ready = true;
-        callback();
-      });
-    } else if (_ready) {
-      _ready = false;
-      callback();
-      _timer = Timer(interval, () {
-        _ready = true;
-      });
+    /// if existing timer is running
+    /// save current callback to call when timer ends
+    if (_timer != null) {
+      latest = callback;
+      return;
     }
+
+    /// directly run callback;
+    callback();
+
+    _timer = Timer(interval, _handleTimerEnd);
+  }
+
+  void _handleTimerEnd() {
+    /// if latest callback is present run it
+    if (latest != null) {
+      latest!();
+      latest = null;
+
+      /// restart timer
+      _timer = Timer(interval, _handleTimerEnd);
+      return;
+    }
+
+    _timer = null;
   }
 
   dispose() {
